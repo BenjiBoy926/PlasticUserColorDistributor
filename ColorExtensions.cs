dad9d3ea8f5619709c749ace3c9472943b84d46a
Codey Huntting <creativecodey@gmail.com>
2023-08-18 09:27:21 -0500

    First commit user color works

---
+++ ColorExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+
+namespace PlasticColorDistributor
+{
+    internal static class ColorExtensions
+    {
+        public const float MaxLuminance = byte.MaxValue;
+        private const float RedLuminanceContribution = 0.2126f;
+        private const float GreenLuminanceContribution = 0.7152f;
+        private const float BlueLuminanceContribution = 0.0722f;
+
+        /// <summary>
+        /// Range from 0 to 255
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static float Luminance(this Color color)
+        {
+            return RedLuminance(color.R) + GreenLuminance(color.G) + BlueLuminance(color.B);
+        }
+        public static Color Parse(string colorString)
+        {
+            return Color.FromArgb(int.Parse(colorString));
+        }
+
+        private static float RedLuminance(int r)
+        {
+            return RedLuminanceContribution * r;
+        }
+        private static float GreenLuminance(int g)
+        {
+            return GreenLuminanceContribution * g;
+        }
+        private static float BlueLuminance(int b)
+        {
+            return BlueLuminanceContribution * b;
+        }
+    }
+}
---
+++ UserColor.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace PlasticColorDistributor
+{
+    internal struct UserColor
+    {
+        private const string Separator = "_@#@_";
+        private static readonly Regex LineSplitter = new Regex(Separator);
+        private const int FieldCount = 4;
+
+        private string _username;
+        private string _clickMessage;
+        private Color _color;
+        private string _guid;
+
+        public static UserColor Parse(string line)
+        {
+            string[] fields = LineSplitter.Split(line);
+            if (fields.Length != FieldCount)
+            {
+                throw new FormatException($"Expected '{line}' to contain {FieldCount} fields, " +
+                    $"but it contains {fields.Length} fields");
+            }
+            return new UserColor
+            {
+                _username = fields[0],
+                _clickMessage = fields[1],
+                _color = ColorExtensions.Parse(fields[2]),
+                // Expecting a possible endline at the end of the last field, so trim it off
+                _guid = fields[3].Trim()
+            };
+        }
+
+        public void SetColor(Color color)
+        {
+            _color = color;
+        }
+        public override string ToString()
+        {
+            return $"UserColor {{ " +
+                $"{nameof(_username)}={_username}, " +
+                $"{nameof(_clickMessage)}={_clickMessage}, " +
+                $"{nameof(_color)}={_color.ToArgb()}, " +
+                $"{nameof(_guid)}={_guid} " +
+                $"}}";
+        }
+        public string ToParsableString()
+        {
+            return $"{_username}{Separator}{_clickMessage}{Separator}{_color.ToArgb()}{Separator}{_guid}";
+        }
+    }
+}
